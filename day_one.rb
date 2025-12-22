0694d69bf192b3de79d10fd0411dd7421f4fd977
Fatuma Tahalil <fatumat6@shopify.com>
2025-12-22 15:53:30 -0500

    updated day one

--- day_one.rb
+++ day_one.rb
@@ -1,18 +1,27 @@
 def change_rotation()
   start = 50
   count = 0
-  File.open('./puzzle_input.txt', "r") do |file_handle|
-    file_handle.each_line do |line|
+  File.open('puzzle_input_one.txt', "r") do |file_handle|
+    file_handle.each_line do |raw|
+      line = raw.strip
+
       turn = line[0]
-      if start == 0
-        count += 1
-      end
+      step = line[1..].to_i
+
+      previous_value = start
       if turn == "R"
-        start = start + line[1..].to_i
+        start += step
       elsif turn == "L"
-        start = start - line[1..].to_i
+        start -= step
       end
-      start %= 100
+
+      lo = [previous_value, start].min
+      hi = [previous_value, start].max
+
+      interior = (hi - 1).div(100) - lo.div(100)
+      endpoint = (start != previous_value && start % 100 == 0) ? 1 : 0
+
+      count += interior + endpoint
     end
   end
   return count
