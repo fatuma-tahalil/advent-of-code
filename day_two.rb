3bdbb6f8c8a53020a858e55378b8e330a16cdc19
Fatuma Tahalil <fatumat6@shopify.com>
2025-12-22 20:42:36 -0500

    complete day 2

--- day_two.rb
+++ day_two.rb
@@ -7,17 +7,22 @@ def invalid_ids
     file_handle.each_line do |line|
       num = line.to_s.split("-")
 
-      start = num[0].to_i
-      last = num[1].to_i
+      start, last = num[0].to_i, num[1].to_i
 
-      if start.to_s.length.even? || last.to_s.length.even?
-        for i in (start..last)
-          length = i.to_s.length
-          if length.even?
-            if i.to_s[..(length/2)-1] == i.to_s[(length/2)..]
-              sum += i
+      if start.to_s.length > 0 # So that we aren't deaing with numbers 0-9
+
+        for number in (start..last)
+          pattern, str_num = "", number.to_s
+
+          str_num.each_char do |char|
+            pattern << char
+            if pattern_true(pattern, str_num)
+              puts number
+              sum += number
+              break
             end
           end
+
         end
       end
 
@@ -26,4 +31,24 @@ def invalid_ids
   return sum
 end
 
-puts invalid_ids
+def pattern_true(pattern, str_num)
+  # Pattern must not be the whole string
+  if pattern == str_num
+    return false
+  end
+  # Pattern must be able to fit in the string
+  if (str_num.length)%(pattern.length) != 0
+    return false
+  end
+  # Pattern must consist of every section of the string
+  len_pattern = pattern.length
+  str_num.scan(/.{1,#{len_pattern}}/) do |section|
+    if section != pattern
+      return false
+    end
+  end
+  return true
+end
+
+
+puts "Answer: #{invalid_ids}"
